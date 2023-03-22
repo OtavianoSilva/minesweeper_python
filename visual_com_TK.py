c143c516fe202b7edf6b7d0b4fe7d2dd1a859ca2
Otaviano Silva <80930262+OtavianoSky@users.noreply.github.com>
2023-03-22 11:44:09 -0300

    sistema de imagens
    utilizando os botões tk como imagens, deixando o jogo mais visual.

--- visual_com_TK.py
+++ visual_com_TK.py
@@ -1,35 +1,42 @@
-from tkinter import *
 from campo_minado import *
+from tkinter import *
+from tkinter.ttk import *
 
 # Responsável por gerenciar os botões como objetos TK.
 class BotaoTK():
     def __init__(self, master: Tk, bloco: Bloco) -> None:
         self.master:        Tk              = master    # A janela a qual o botão pertence,
         self.bloco:         Bloco           = bloco     # o bloco referenciado
-        self.cores:         dict[str / int] = {         # grade de possiveis conteúdos
-            'aberto'  :     'gray',                     # do botão, e sua representação.
-            'fechado' :     'white',
-            'mina'    :     'red',
-            'bandeira':     'yellow',
-            1         :     'blue',
-            2         :     'green',
-            3         :     'red',
-            4         :     'purple',                   # Ps.:
-            5         :     'maroon',                   # Toda a apresentação visual é um espelho
-            6         :     'turquoise',                # do botão pelo qual a classe se baseia
-            7         :     'black',
-            8         :     'gray',
-        }
 
+    ### Métodos do Botao ###
     # Cria o botão como objeto Button
     def cria_botao_visual(self) -> None:
-        self.botao = Button(self.master, text='+' if self.bloco.bandeira else f'{bloco.conteudo}' if self.bloco.aberto else '  ')
-        self.botao.grid(column=self.bloco.coluna, row=self.bloco.linha)
+        imagem = self.define_imagem()        
+
+        self.botao = Button(self.master, image=imagem)
+        self.botao.grid(column=self.bloco.coluna, row=self.bloco.linha+1)
         if not self.bloco.bandeira:
             self.botao.bind('<Button-1>', self.revela_botao)            # Ao toque direito, abre o botão,
             self.botao.bind('<Button-3>', self.adiciona_bandeira_botao) # ao toque esquerdo, adiciona bandeira.
         else:                                                           # E caso já tenha banderia, ao toque
             self.botao.bind('<Button-3>', self.remove_bandeira_botao)   # esquerdo, a remove.
+
+    def define_imagem(self) -> Image:                                   # Define qual a imagem que deve
+        if self.bloco.bandeira:                                         # ser apresentada no botão
+            controle: str = 'bandeira.png'
+        elif self.bloco.conteudo >= 9 and self.bloco.aberto:
+            controle: str = 'mina.png'
+        elif self.bloco.aberto and self.bloco.conteudo != 0:
+            controle: str = f'{self.bloco.conteudo}.png'
+        elif self.bloco.aberto:
+            controle: str = 'aberto.png'
+        else: controle: str = 'campo.png'
+
+        # Define a imagem, seguindo as condições acima
+        imagem = PhotoImage(file=f'imagens/{controle}').subsample(30, 30)
+        figura = Label(self.master, image=imagem)
+        figura.image = imagem
+        return imagem
 
     # Todos recebem o paramentro 'event' pela funcionalidade da função bind
     def revela_botao(self, event) -> cria_botao_visual:
@@ -48,21 +55,70 @@ class BotaoTK():
         return self.cria_botao_visual() # O return pode causar recursão, caso de erro, é aqui
 
 # Gerencia a apresentação visual do campo com TK
-class CampoTK():
+class CampoTK(Campo):
+    def __init__(self, linhas: int, colunas: int, minas: int) -> None:
+        super().__init__(linhas, colunas, minas)
+        self.cria_campo()
+
+        ### Propriedades da janela campo ###
+        self.janela_campo:    Tk  = Tk()
+        self.janela_campo.title('Campo Minado')
+        self.janela_campo.iconbitmap('imagens/soriso.ico')
+
+        self.contador_bandeiras()
+        self.cria_campo_visual()
+
+    ### Métodos do campo ###
+    def contador_bandeiras(self) -> None:
+
+        imagem: PhotoImage  = PhotoImage(file='imagens/bandeira.png').subsample(46, 46)
+        figura: Label       = Label(self.janela_campo, image=imagem)
+        figura.image = imagem
+
+        lable_bandeiras = Label(self.janela_campo, text=f'{self.minas}', image=imagem, compound=RIGHT,
+                                font=('Arial', 10)).grid(column=1, row=0, pady=6)
+
+    def cria_campo_visual(self) -> None:
+
+        for bloco in self.campo.values():               # Um a um passa pelos blocos
+            botoes = BotaoTK(self.janela_campo, bloco)  # criando seu objeto tk equivalente
+            botoes.cria_botao_visual()
+        
+        self.janela_campo.mainloop()
+
+class JanelaPerdaTK():
     def __init__(self) -> None:
-
-        self.janela = Tk()
-        self.janela.title('Campo Minado')
-        self.janela.geometry(f'{9*self.largura_bloco+4}x{9*self.altura_bloco+100}')
-        self.frame = Frame(self.janela)
-        self.frame.pack()
+        self.janela_perda:    Tk  = Tk()
+        self.janela_perda.title('Perda')
+        self.janela_perda.iconbitmap('imagens/triste.ico')
 
 
-        self.janela.mainloop()
+class MenuTK():
+    def __init__(self) -> None:
+        self.janela_menu:   Tk      =   Tk()
+        texto:              Label   =   Label(self.janela_menu,
+                                              text='Escolha seu modo de jogo:').pack()
 
+        ### Propriedades da janela menu ###
+        self.janela_menu.title("Menu")
+        self.janela_menu.geometry('400x600+50+50')
+        self.janela_menu.resizable(False, False)
+        self.janela_menu.iconbitmap('imagens/soriso.ico')
 
-bloco = Bloco(9, 9, 0, False, False)
-janela = Tk()
-teste = BotaoTK(janela, bloco)
-teste.cria_botao_visual()
-janela.mainloop()
+        ### Botões que inicializam os jogos ###
+        botao_facil = Button(self.janela_menu, text='fácil\n9x9 10 minas',
+                            command= lambda: self.inicia_jogo(9,9,10)).pack()
+        botao_medio = Button(self.janela_menu, text='Medio\n16x16 30 minas',
+                            command= lambda: self.inicia_jogo(16,16,40)).pack()
+        botao_dificil = Button(self.janela_menu, text='Dificil\n16x30 99 minas',
+                            command= lambda: self.inicia_jogo(16,30,99)).pack()
+
+        self.janela_menu.mainloop()
+
+    ### Métodos do menu ###
+    def inicia_jogo(self, l: int, c: int, m: int) -> None:
+        self.janela_menu.destroy()
+        CampoTK(l, c, m)
+
+if __name__ == "__main__": 
+    menu = MenuTK()
